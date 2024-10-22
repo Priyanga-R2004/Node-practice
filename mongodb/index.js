9339a0e8d3755f180ddfc0d229cb9a7968d09835
Priyanga-R2004 <priyangaraja04@gmail.com>
2024-10-22 18:01:06 +0530

    mongodb schema,model creation

---
+++ mongodb/index.js
@@ -0,0 +1,51 @@
+const mongoose =require('mongoose');
+mongoose.connect('mongodb://localhost/playground')
+ .then(()=>console.log('connected to MongoDB'))
+ .catch(err=>console.log('Error: '+err.message));
+
+ const courseSchema = new mongoose.Schema(
+    {
+        name: String,
+        author: String,
+        tags:[String],
+        date:{type:Date,default:Date.now},
+        isPublished: Boolean
+    }
+ )
+
+ //here Course is the class
+ const Course = mongoose.model('Course',courseSchema);   //first argument is collection name,
+
+ async function createCourse(params) {
+    
+    const course = new Course({
+    name:'Angular Course',
+    author:'Mosh',
+    tags:['angular','frontend'],
+    isPublished:true
+ });
+ const result = await course.save();
+ console.log(result);
+ }
+ 
+
+ //To get document for db
+
+ 
+  function getCourses(){
+    const courses=  Course
+        .find({author:'Mosh'})
+        .limit(10)
+        .sort({name:1})
+        .select({name:1,tags:1});
+   //const arr=['a1','a2'];
+    return courses;
+ }
+
+
+ //createCourse();
+var c=getCourses();
+console.log(c);
+
+
+
