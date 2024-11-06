79c01f3dfb6d96f6860f031e6223fe7ac4ee900b
Priyanga-R2004 <priyangaraja04@gmail.com>
2024-11-06 18:53:56 +0530

    Error logging

--- vidly/middleware/error.js
+++ vidly/middleware/error.js
@@ -4,14 +4,5 @@ module.exports = function(err,req,res,next){
 
   winston.error(err.message,err);
     res.status(500).send('something failed..');
-  
-  /*
-    error
-    warn
-    info
-    verbose
-    debug
-    silly
-  */
   }
   
