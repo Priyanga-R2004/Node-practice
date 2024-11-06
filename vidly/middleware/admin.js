88019a93c8e799dca4bd668a095ee11a85b6b130
Priyanga-R2004 <priyangaraja04@gmail.com>
2024-11-06 18:52:49 +0530

    admin athentication

--- vidly/middleware/admin.js
+++ vidly/middleware/admin.js
@@ -2,7 +2,7 @@
 module.exports = function (req, res, next) { 
   // 401 Unauthorized
   // 403 Forbidden 
-  
+  console.log(req.user, 'reqqqqq');
   if (!req.user.isAdmin) return res.status(403).send('Access denied.');
 
   next();
