0ef76f5f31016e258cff0d49875309aa2f125e1d
Priyanga-R2004 <priyangaraja04@gmail.com>
2024-11-06 18:53:24 +0530

    authentication using token

--- vidly/middleware/auth.js
+++ vidly/middleware/auth.js
@@ -3,14 +3,18 @@ const config = require('config');
 
 module.exports = function (req, res, next) {
   const token = req.header('x-auth-token');
+  console.log(token, 'tokennn')
   if (!token) return res.status(401).send('Access denied. No token provided.');
 
   try {
+    console.log(config, 'config',  config.get('jwtPrivateKey'))
     const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
+    console.log(decoded, 'decoded');
     req.user = decoded; 
     next();
   }
   catch (ex) {
+    console.log(ex, 'ex')
     res.status(401).send('Invalid token.');
   }
 }
